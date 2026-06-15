7c53409c55ff471cefb87c5ae276ff89afea1e3f
Henrique Rotta <henrique.zell@gmail.com>
2026-06-14 21:09:44 -0300

    fixed: next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['@clerk/nextjs', '@clerk/backend'],
 };
 
 export default nextConfig;
